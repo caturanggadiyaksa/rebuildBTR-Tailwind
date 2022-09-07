1979bb3c89f3270bed3200ea8161c54176243f5a
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2022-09-07 21:23:07 +0700

    BTR Web With Tailwind

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,3 +21,85 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+
+//All Mathces
+function mathcesAll() {
+  const allMat = document.querySelector('#all-matches');
+  const pubgMobile = document.querySelector('#macthes-pubgmobile');
+  const mobileLegends = document.querySelector('#matches-mobilelegends');
+
+  pubgMobile.classList.remove('block');
+  pubgMobile.classList.add('hidden');
+
+  mobileLegends.classList.remove('block');
+  mobileLegends.classList.add('hidden');
+
+
+  allMat.classList.remove('hidden');
+  allMat.classList.add('block');
+
+}
+
+
+//mathches PUBG
+function macthesPubg() {
+  const pubgMobile = document.querySelector('#macthes-pubgmobile');
+  const alMathces = document.querySelector('#all-matches');
+  const mobileLegends = document.querySelector('#matches-mobilelegends');
+
+
+  mobileLegends.classList.remove('block');
+  mobileLegends.classList.add('hidden');
+  alMathces.classList.add('hidden');
+  alMathces.classList.remove('block');
+  pubgMobile.classList.remove('hidden');
+  pubgMobile.classList.add('block');
+}
+
+
+
+
+//matches Mobile legends
+function matchesML() {
+  const mobileLegends = document.querySelector('#matches-mobilelegends');
+  const allMatches = document.querySelector('#all-matches');
+  const pubgMobile = document.querySelector('#macthes-pubgmobile');
+
+
+  pubgMobile.classList.remove('block');
+  pubgMobile.classList.add('hidden');
+  allMatches.classList.add('hidden');
+  mobileLegends.classList.remove('hidden');
+  mobileLegends.classList.add('block');
+}
+
+
+//slide show
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  //let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+  slides[slideIndex-1].style.display = "block";  
+  //dots[slideIndex-1].className += " active";
+}
